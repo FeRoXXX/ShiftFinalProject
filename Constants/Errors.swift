2b73a75f975f2270a9ea1ecd3198422917e7bbde
Александр Федоткин <ben115899@gmail.com>
2024-06-16 19:32:14 +0700

    Add Errors constants

---
+++ Constants/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  ShiftFinalProject
+//
+//  Created by Александр Федоткин on 16.06.2024.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case serverError
+    case clientError
+    case newDomain
+}
